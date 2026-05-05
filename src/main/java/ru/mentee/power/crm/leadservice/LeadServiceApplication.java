5af717008b307d260606dd79767f579ffdb68b12
ivental <iventalll@gmail.com>
2026-05-05 20:20:53 +0300

    Начало сборки проекта

---
+++ src/main/java/ru/mentee/power/crm/leadservice/LeadServiceApplication.java
@@ -0,0 +1,11 @@
+package ru.mentee.power.crm.leadservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadServiceApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(LeadServiceApplication.class);
+  }
+}
